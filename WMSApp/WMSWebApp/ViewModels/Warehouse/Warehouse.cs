b4bf8d3e22140d15d3bbfb1924a2bc6c6c1b1d6f
Rajendra Hiray <raj.hiray@outlook.com>
2022-05-08 23:20:16 +0530

    Warehouse added.

---
+++ WMSApp/WMSWebApp/ViewModels/Warehouse/Warehouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WMSWebApp.ViewModels
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+        public string WarehouseCode { get; set; }
+        public string WarehouseName { get; set; }
+        public string Location { get; set; }
+        public string ContactMaintainenceIncharge { get; set; }
+        public string ContactNumberIncharge { get; set; }
+        public string TotalSpaceSizeType { get; set; }
+        public string TotalSpaceSize { get; set; }
+        List<WarehouseZone> ZoneList { get; set; }
+        List<WarehouseAisle> AisleList { get; set; }
+        List<WarehouseArea> AreasList { get; set; }
+    }
+}
