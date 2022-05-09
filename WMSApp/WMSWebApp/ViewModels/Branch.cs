bf170b4cf355a7a19194bb5a4582cd1381caf7b3
RAHUL JHA <rahuljha8849@gmail.com>
2022-05-09 15:45:21 +0530

    UI change commit test

--- WMSApp/WMSWebApp/ViewModels/Branch.cs
+++ WMSApp/WMSWebApp/ViewModels/Branch.cs
@@ -4,21 +4,39 @@ namespace WMSWebApp.ViewModels
 {
     public class Branch
     {
-        
+
+        [Display(Name = "Screen Code")]
         public string ScreenCode { get; set; }
+        [Display(Name = "Branch Code")]
         public string BranchCode { get; set; }
+
+        [Display(Name = "Branch Name")]
         public string BranchName { get; set; }
+        [DataType(DataType.MultilineText)]
+
+        [Display(Name = "Company Name")]
         public string CompanyName { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Address { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Location { get; set; }
+
+        [Display(Name = "Branch Contact Person")]
         public string ContactPersonBranch { get; set; }
+        [DataType(DataType.MultilineText)]
         [Required]
         [Phone]
+        [Display(Name = "Branch Contact Number")]
         public string ContactNumberBranch { get; set; }
         [Required]
         [EmailAddress]
+        [Display(Name = "Branch Email ID")]
         public string EmailIdBranch { get; set; }
+
+        [Display(Name = "Associated Employee")]
         public string AssociatedEmployee { get; set; }
+
+        [Display(Name = "Warehouse Name")]
         public string WarehouseName { get; set; }
 
     }
