8cefe587b62c2f0f483a0d2491657a5879aea142
Dharmesh Sharma <dharmesh_sharma15@live.com>
2022-05-24 12:44:17 +0530

    Data table added for UI

---
+++ WMSApp/WMSWebApp/Controllers/GrnController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WMSWebApp.Controllers
+{
+    public class GrnController : Controller
+    {
+        #region Fields
+
+        #endregion
+
+        #region Ctor
+
+        #endregion
+
+        #region Methods
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
