fefdb17b3e17fb88a7fd207d298c463337107753
Dharmesh Sharma <55456495+Dharmesh-IT@users.noreply.github.com>
2022-10-21 22:32:40 +0530

    Subitem ampping si done

--- WMSApp/WMSWebApp/Controllers/SubItemMappingController.cs
+++ WMSApp/WMSWebApp/Controllers/SubItemMappingController.cs
@@ -1,6 +1,10 @@
-﻿using Application.Services.Master;
+﻿using Application.Services;
+using Application.Services.Master;
+using Domain.Model.SubItemMapping;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using WMS.Web.Framework.Infrastructure.Extentsion;
 using WMSWebApp.ViewModels.SubItemMapping;
 
@@ -11,12 +15,14 @@ namespace WMSWebApp.Controllers
     {
         #region Fields
         private readonly ISubItemWarehouseMappingService _subItemWarehouseMappingService;
+        private readonly ISubItemHelper _subItemService;
         #endregion
 
         #region Ctor
-        public SubItemMappingController(ISubItemWarehouseMappingService subItemWarehouseMappingService)
+        public SubItemMappingController(ISubItemWarehouseMappingService subItemWarehouseMappingService, ISubItemHelper subItemService)
         {
             _subItemWarehouseMappingService = subItemWarehouseMappingService;
+            _subItemService = subItemService;
         }
         #endregion
 
@@ -39,7 +45,30 @@ namespace WMSWebApp.Controllers
         public IActionResult Create()
         {
             SubItemMappingModel model = new SubItemMappingModel();
+
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(SubItemMappingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                SubItemWareHouseMapping mapping = new SubItemWareHouseMapping();
+                mapping.WareHouseAreaId = model.WareHouseAreaId;
+                mapping.LocationId = model.LocationId;
+                mapping.SubItemCode = model.SubItemCode;
+                mapping.WareHouseId = model.WareHouseId;
+                mapping.CreateOn = DateTime.Now;
+                _subItemWarehouseMappingService.Insert(mapping);
+                SuccessNotification("Subitem successfully mapped");
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return View(model);
+            }
+
         }
 
         public IActionResult Edit(int id)
@@ -54,7 +83,25 @@ namespace WMSWebApp.Controllers
             return View();
         }
 
+
+
+        [HttpGet]
+        public IActionResult GetSubItem()
+        {
+            var subitems = _subItemService.GetSubItem("0");
+            List<SubItemListModel> list = new List<SubItemListModel>();
+            foreach (var item in subitems)
+            {
+                SubItemListModel model = new SubItemListModel();
+                model.Name = $"{item.SubItemName} ({item.SubItemCode})";
+                model.Code = item.SubItemCode;
+                list.Add(model);
+            }
+            return Json(list);
+        }
         #endregion
+
+
 
     }
 }
--- WMSApp/WMSWebApp/wwwroot/js/subitemmapping/mapping.js
+++ WMSApp/WMSWebApp/wwwroot/js/subitemmapping/mapping.js
@@ -5,7 +5,7 @@ $(function () {
         /* minimumInputLength: 2,*/
         /*templateResult: formatState, //this is for append country flag.*/
         ajax: {
-            url: '/Grn/PendingPO',
+            url: '/SubItemMapping/GetSubItem',
             dataType: 'json',
             type: "GET",
             //data: function (term) {
@@ -18,8 +18,8 @@ $(function () {
                 return {
                     results: $.map(data, function (item) {
                         return {
-                            text: item,
-                            id: item,
+                            text: item.Name,
+                            id: item.Code,
                         }
                     })
                 };
@@ -30,3 +30,87 @@ $(function () {
 
 
 });
+
+
+$(document).ready(function () {
+
+    warehouse();
+    $("#WareHouseId").change(function () {
+        var element = $(this).find('option:selected');
+        var myTag = element.attr("data-code");
+
+       
+    });
+    $("#WareHouseId").trigger("change");
+    $("#WareHouseAreaId").change(function () {
+        var element = $(this).find('option:selected');
+        var myTag = element.attr("data-code");
+
+       
+        area($("#WareHouseId").val(), $("#WareHouseAreaId").val());
+    });
+    $("#WareHouseAreaId").trigger("change");
+
+    $("#LocationId").change(function () {
+        var element = $(this).find('option:selected');
+        var myTag = element.attr("data-code");
+
+        
+
+    });
+    $("#LocationId").trigger("change");
+});
+
+function warehouse() {
+    $.ajax({
+        type: "GET",
+        url: "/Grn/Warehouse",
+        async: false,
+        success: function (data) {
+            var s = '';
+            for (var i = 0; i < data.length; i++) {
+                s += '<option value="' + data[i].Id + '" data-code=' + data[i].WarehouseCode + '>' + data[i].WarehouseName + '</option>';
+            }
+            $("#WareHouseId").html(s);
+        }
+    });
+
+
+    zones($("#WareHouseId").val());
+
+}
+
+function zones(warehouseId) {
+    $.ajax({
+        type: "GET",
+        url: "/Grn/WarehouseZone?warehouseid=" + warehouseId,
+        data: "{}",
+        success: function (data) {
+            var s = '';
+            for (var i = 0; i < data.length; i++) {
+                s += '<option value="' + data[i].Id + '" data-code=' + data[i].ZoneCode + '>' + data[i].ZoneName + '</option>';
+            }
+            $("#WareHouseAreaId").html(s);
+            $("#zoWareHouseAreaIdne").trigger("change");
+        }
+    });
+
+    area($("#WareHouseId").val(), $("#WareHouseAreaId").val());
+
+}
+function area(warehouseId, zoneid) {
+    $.ajax({
+        type: "GET",
+        url: "/Grn/WarehouseArea?warehouseid=" + warehouseId + '&zoneid=' + zoneid,
+        data: "{}",
+        success: function (data) {
+            var s = '';
+            for (var i = 0; i < data.length; i++) {
+                s += '<option value="' + data[i].Id + '" data-code=' + data[i].AreaCode + '  data-size=' + data[i].Size + '>' + data[i].AreaName + '</option>';
+            }
+            $("#LocationId").html(s);
+            $("#LocationId").trigger("change");
+        }
+    });
+
+}
